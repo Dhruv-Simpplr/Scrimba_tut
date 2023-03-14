4056fc2d37432b440c142ff1adbc8f212d56ae8f
Dhruv Vashist <dhruv.vashist@simpplr.com>
2023-03-14 22:02:06 +0530

    14 March Update From MAC

--- scrimba_tutorial/src/App.js
+++ scrimba_tutorial/src/App.js
@@ -1,7 +1,23 @@
+import logo from './science.png';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <header>
+        <nav>
+          <img src={logo} alt="React Logo" width="40px" />
+        </nav>
+      </header>
+      <h1>This is Component</h1>
+      <ol>
+        <li>
+          It's a popular library, so I'll be able to fit in with the cool kids!
+        </li>
+        <li>I'm more likely to get a job as a developer if I know React</li>
+      </ol>
+      <footer>
+        <small>© 2023 Scrimba_tu development. All rights reserved.</small>
+      </footer>
     </div>
   );
 }
