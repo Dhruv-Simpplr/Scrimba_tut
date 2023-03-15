91d725c0e11a4bb9ae8428561f152d544936e7c2
Dhruv Vashist <dhruv.vashist@simplr.com>
2023-03-15 23:04:20 +0530

    Added the backgorund image

--- scrimba_tutorial/src/Page.js
+++ scrimba_tutorial/src/Page.js
@@ -1,6 +1,15 @@
+import bckg from './resizedbckg.png';
 function Page() {
   return (
-    <div className="page-div">
+    <div
+      className="page-div"
+      style={{
+        backgroundImage: `url(${bckg})`,
+        backgroundPositionX: '87vw',
+        backgroundPositionY: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
       <h1>Fun Facts About React</h1>
       <ul className="page--facts">
         <li>Was first released in 2013</li>
